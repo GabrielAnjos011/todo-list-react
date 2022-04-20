c12bf176c0bbae9ab763b60b37dc0f985aac0b26
GabrielAnjos011 <gabrielgarcia.anjos250@gmail.com>
2022-04-20 08:56:26 -0300

    Criação de formulario + rota de POST

--- src/App.js
+++ src/App.js
@@ -11,13 +11,60 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false
+    }
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+
+    setTitle("")
+    setTime("")
+  }
+
   return (
     <div className="App">
       <div className='todo-header'>
         <h1>React Todo</h1>
       </div>
       <div className='form-todo'>
-        <p>Formulario</p>
+        <h2>Insira a sua proxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input 
+              type="text" 
+              name='title' 
+              placeholder='Título da tarefa' 
+              onChange={(e) => setTitle(e.target.value)} 
+              value={title ? title : ''}
+              required
+            />
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input 
+              type="text" 
+              name='time' 
+              placeholder='Tempo estimado (em horas)' 
+              onChange={(e) => setTime(e.target.value)} 
+              value={time ? time : ''}
+              required
+            />
+          </div>
+            <input type="submit" value="Criar tarefa" />
+          </div>
+        </form>
       </div>
       <div className='list-todo'>
         <h2>Listas de tarefas:</h2>
